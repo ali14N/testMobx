e9bc243f6e5699524e56cd8c841f15f97e121b8d
Nadir Ali <alinadir0509@gmail.com>
2020-05-20 10:44:33 +0530

    todoList working

--- src/index.js
+++ src/index.js
@@ -1,39 +1,83 @@
-import React, { Component } from 'react';
-import ReactDOM from "react-dom"
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { observable, computed } from 'mobx'
 import { observer } from 'mobx-react'
-import { observable, computed } from "mobx"
-
-class TodoList {
-  @observable todos = []
-  @computed get unfinishedTodoCount() {
-    return this.todos.filter(todo => !todo.finished).length
+import DevTools from 'mobx-react-devtools'
+class Todo {
+  id = Math.random();
+  @observable title;
+  @observable finished = false
+  constructor(title) {
+    this.title = title
   }
 }
+
+class TodoList {
+  @observable todos = [];
+  @computed get unfinishedTodoCount() {
+    return this.todos.filter(todo => !todo.finished).length;
+  }
+}
+
 @observer
 class TodoListView extends Component {
   render() {
+    return <div>
+      <ul>
+        {this.props.todoList.todos.map(todo =>
+          <TodoView todo={todo} key={todo.id} />
+        )}
+      </ul>
+      Tasks left: {this.props.todoList.unfinishedTodoCount}
+      <TodoAdd todoList={store} />
+      <DevTools />
+    </div>
+  }
+}
+
+const TodoView = observer(({ todo }) =>
+  <li>
+    <input type='checkbox'
+      checked={todo.finished}
+      onChange={() => todo.finished = !todo.finished}
+    />
+    {todo.title}
+  </li>
+)
+
+@observer
+class TodoAdd extends Component {
+  state = {
+    inputText: ""
+  }
+  render() {
+    const inputHandler = (e) => {
+      const inputText = e.target.value
+      this.setState({ inputText })
+    }
+    const submitHandler = (e) => {
+      e.preventDefault();
+      console.log(this.state.inputText)
+      this.props.todoList.todos.push(new Todo(this.state.inputText))
+      console.log(this.props.todoList.todos)
+    }
     return (
-      <div>
-        <ul>
-          {this.props.todoList.todo.map(todo => (
-            <TodoView todo={todo} key={todo.id} />
-          ))}
-        </ul>
-                Tasks left: {this.props.todoList.unfinishedTodoCount}
-      </div>
+      <form onSubmit={submitHandler}>
+        <input type="text"
+          onChange={inputHandler}
+        />
+        <button type="submit">Add</button>
+      </form>
     )
   }
 }
 
-const TodoView = observer(({ todo }) => (
-  <li>
-    <input
-      type="checkbox"
-      checked={todo.finished} onClick={() => (todo.finished = !todo.finished)}
-    />
-    {todo.title}
-  </li>
-))
-
 const store = new TodoList()
-ReactDOM.render(<TodoListView todoList={store} />, document.getElementById("root"))
+
+ReactDOM.render(<TodoListView todoList={store} />, document.getElementById('root'))
+
+store.todos.push(
+  new Todo("understand how counter works in mobx"),
+  new Todo("add items to Todo List"),
+);
+store.todos[0].finished = true;
